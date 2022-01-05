36b9ee133bc18abaf0ad7455671d88f36675687b
gcctbilly <gcctxhl@163.com>
2022-01-05 12:24:27 +0800

    add the comment

--- workflows/src/main/kotlin/hk/edu/polyu/af/bc/badge/flows/UpdateBadgeClass.kt
+++ workflows/src/main/kotlin/hk/edu/polyu/af/bc/badge/flows/UpdateBadgeClass.kt
@@ -1,15 +1,31 @@
 package hk.edu.polyu.af.bc.badge.flows
 
 import co.paralleluniverse.fibers.Suspendable
+import com.r3.corda.lib.tokens.workflows.flows.rpc.CreateEvolvableTokens
 import com.r3.corda.lib.tokens.workflows.flows.rpc.UpdateEvolvableToken
 import hk.edu.polyu.af.bc.badge.states.BadgeClass
 import net.corda.core.contracts.StateAndRef
 import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.flows.StartableByService
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 
+/**
+ * Update a [BadgeClass].
+ *
+ * This flow is used to update an existed BadgeClass
+ *
+ * @property badgeClassRef the reference of an existed BadgeClass
+ * @property name name of this type of badges
+ * @property description a short description of achievement denoted by this type of badges
+ * @property image the byte code of image
+ * @property observers observer parties (optional)
+ */
 
+@StartableByRPC
+@StartableByService
 class UpdateBadgeClass(
     private val badgeClassRef: StateAndRef<BadgeClass>,
     private val name: String,
