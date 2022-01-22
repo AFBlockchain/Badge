f6363b8ce95fbb9fd35a6be4be2b07e9149b39e8
YANGZhaochen2002 <93120280+YANGZhaochen2002@users.noreply.github.com>
2022-01-22 15:19:07 +0800

    Update ReadAssertionTest.kt

--- workflows/src/test/kotlin/hk/edu/polyu/af/bc/badge/assertion/ReadAssertionTest.kt
+++ workflows/src/test/kotlin/hk/edu/polyu/af/bc/badge/assertion/ReadAssertionTest.kt
@@ -1,7 +1,9 @@
 package hk.edu.polyu.af.bc.badge.badgeclass
 import hk.edu.polyu.af.bc.badge.UnitTestBase
+import hk.edu.polyu.af.bc.badge.flows.assertion.GetAssertionByBadgeClass
 import hk.edu.polyu.af.bc.badge.flows.assertion.IssueAssertion
 import hk.edu.polyu.af.bc.badge.flows.assertion.GetAssertionByID
+import hk.edu.polyu.af.bc.badge.flows.assertion.GetAssertionByRecipient
 import hk.edu.polyu.af.bc.badge.flows.badgeclass.CreateBadgeClass
 import hk.edu.polyu.af.bc.badge.getOrThrow
 import hk.edu.polyu.af.bc.badge.output
@@ -36,5 +38,40 @@ class ReadAssertionTest : UnitTestBase() {
         val result=instA.startFlow(GetAssertionByID(uuid)).getOrThrow(network)
         assertEquals(assertion,result)
         
+    }
+    @Test
+    fun `can read by recipient`() {
+        // create a BadgeClass
+        logger.info("Creating BadgeClass");
+        val badgeClassTx = instA.startFlow(CreateBadgeClass("test", "test", ByteArray(1))).getOrThrow(network)
+        val badgeClass = badgeClassTx.output(BadgeClass::class.java)
+        logger.info("BadgeClass Created: {}", badgeClass);
+
+        // issue an Assertion
+        logger.info("Issuing assertion")
+        val assertionTx = instA.startFlow(IssueAssertion(badgeClass.toPointer(), learnerA.info.legalIdentities[0])).getOrThrow(network)
+        val assertion = assertionTx.output(Assertion::class.java)
+        val recipient=assertion.participants[1]
+        val result=instA.startFlow(GetAssertionByRecipient(recipient)).getOrThrow(network)
+        val final=result.get(0).state.data
+        assertEquals(assertion,final)
+
+    }
+    @Test
+    fun `can read by badgeclass`() {
+        // create a BadgeClass
+        logger.info("Creating BadgeClass");
+        val badgeClassTx = instA.startFlow(CreateBadgeClass("test", "test", ByteArray(1))).getOrThrow(network)
+        val badgeClass = badgeClassTx.output(BadgeClass::class.java)
+        logger.info("BadgeClass Created: {}", badgeClass);
+
+        // issue an Assertion
+        logger.info("Issuing assertion")
+        val assertionTx = instA.startFlow(IssueAssertion(badgeClass.toPointer(), learnerA.info.legalIdentities[0])).getOrThrow(network)
+        val assertion = assertionTx.output(Assertion::class.java)
+        val result=instA.startFlow(GetAssertionByBadgeClass(badgeClass)).getOrThrow(network)
+        val final=result.get(0).state.data
+        assertEquals(assertion,final)
+
     }
 }
