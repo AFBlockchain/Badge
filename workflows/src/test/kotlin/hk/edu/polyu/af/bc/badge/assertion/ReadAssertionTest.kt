9d535419829d941519fb39e6b0eb580e9037bf53
YANGZhaochen2002 <93120280+YANGZhaochen2002@users.noreply.github.com>
2022-01-22 15:06:18 +0800

    Update ReadAssertionTest.kt
    pass

--- workflows/src/test/kotlin/hk/edu/polyu/af/bc/badge/assertion/ReadAssertionTest.kt
+++ workflows/src/test/kotlin/hk/edu/polyu/af/bc/badge/assertion/ReadAssertionTest.kt
@@ -1,25 +1,17 @@
 package hk.edu.polyu.af.bc.badge.badgeclass
-
 import hk.edu.polyu.af.bc.badge.UnitTestBase
-import hk.edu.polyu.af.bc.badge.assertHaveState
-import hk.edu.polyu.af.bc.badge.assertion.IssueAssertionTest
 import hk.edu.polyu.af.bc.badge.flows.assertion.IssueAssertion
-import hk.edu.polyu.af.bc.badge.flows.assertion.ReadAssertionByID
+import hk.edu.polyu.af.bc.badge.flows.assertion.GetAssertionByID
 import hk.edu.polyu.af.bc.badge.flows.badgeclass.CreateBadgeClass
-import hk.edu.polyu.af.bc.badge.flows.badgeclass.ReadAllBadgeClass
-import hk.edu.polyu.af.bc.badge.flows.badgeclass.ReadBadgeClassById
-import hk.edu.polyu.af.bc.badge.flows.badgeclass.ReadBadgeClassByName
 import hk.edu.polyu.af.bc.badge.getOrThrow
 import hk.edu.polyu.af.bc.badge.output
 import hk.edu.polyu.af.bc.badge.states.Assertion
 import hk.edu.polyu.af.bc.badge.states.BadgeClass
-import net.corda.core.contracts.StateAndRef
-import net.corda.core.utilities.getOrThrow
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.slf4j.LoggerFactory
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ReadAssertionTest : UnitTestBase() {
@@ -41,7 +33,7 @@ class ReadAssertionTest : UnitTestBase() {
         val assertionTx = instA.startFlow(IssueAssertion(badgeClass.toPointer(), learnerA.info.legalIdentities[0])).getOrThrow(network)
         val assertion = assertionTx.output(Assertion::class.java)
         val uuid=assertion.linearId.id
-        val result=instA.startFlow(ReadAssertionByID(uuid)).getOrThrow(network)
+        val result=instA.startFlow(GetAssertionByID(uuid)).getOrThrow(network)
         assertEquals(assertion,result)
         
     }
