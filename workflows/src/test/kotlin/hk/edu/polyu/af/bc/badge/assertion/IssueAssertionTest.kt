90f4f8bd8e2756dfbd700651fb1c6137f055c93b
YANGZhaochen2002 <93120280+YANGZhaochen2002@users.noreply.github.com>
2022-01-22 14:55:24 +0800

    Update IssueAssertionTest.kt
    issueAsseriontest (All pass)

--- workflows/src/test/kotlin/hk/edu/polyu/af/bc/badge/assertion/IssueAssertionTest.kt
+++ workflows/src/test/kotlin/hk/edu/polyu/af/bc/badge/assertion/IssueAssertionTest.kt
@@ -4,6 +4,8 @@ import hk.edu.polyu.af.bc.badge.UnitTestBase
 import hk.edu.polyu.af.bc.badge.assertHaveState
 import hk.edu.polyu.af.bc.badge.badgeclass.CreateBadgeClassTest
 import hk.edu.polyu.af.bc.badge.flows.assertion.IssueAssertion
+import hk.edu.polyu.af.bc.badge.flows.assertion.IssueAssertionByID
+import hk.edu.polyu.af.bc.badge.flows.assertion.IssueAssertionByName
 import hk.edu.polyu.af.bc.badge.flows.badgeclass.CreateBadgeClass
 import hk.edu.polyu.af.bc.badge.getOrThrow
 import hk.edu.polyu.af.bc.badge.output
@@ -40,6 +42,61 @@ class IssueAssertionTest: UnitTestBase() {
             s1.linearId == s2.linearId
         }
         learnerA.assertHaveState(assertion) {
+            s1, s2 ->
+            s1.linearId == s2.linearId
+        }
+    }
+
+    @Test
+    fun `InstitutionA issues an Assertion to LearnerA by uuid`() {
+        // create a BadgeClass
+        logger.info("Creating BadgeClass");
+        val badgeClassTx = instA.startFlow(CreateBadgeClass("test", "test", ByteArray(1))).getOrThrow(network)
+        val badgeClassTxB = instB.startFlow(CreateBadgeClass("test1", "test1", ByteArray(1))).getOrThrow(network)
+        val badgeClass = badgeClassTx.output(BadgeClass::class.java)
+        val badgeClassB = badgeClassTxB.output(BadgeClass::class.java)
+        logger.info("BadgeClass Created: {}", badgeClass);
+
+        // issue an Assertion
+        logger.info("Issuing assertion")
+        val assertionTx = instA.startFlow(IssueAssertion(badgeClass.toPointer(), learnerA.info.legalIdentities[0])).getOrThrow(network)
+        val uuid=badgeClassB.linearId.id
+        logger.info("Issuing assertion")
+        val assertionTxB = instB.startFlow(IssueAssertionByID(uuid, learnerA.info.legalIdentities[0])).getOrThrow(network)
+        val assertionB = assertionTxB.output(Assertion::class.java)
+        logger.info("Assertion issued: {}", assertionB.toString())
+        instB.assertHaveState(assertionB) {
+            s1, s2 ->
+            s1.linearId == s2.linearId
+        }
+        learnerA.assertHaveState(assertionB) {
+            s1, s2 ->
+            s1.linearId == s2.linearId
+        }
+    }
+    @Test
+    fun `InstitutionA issues an Assertion to LearnerA by name`() {
+        // create a BadgeClass
+        logger.info("Creating BadgeClass");
+        val badgeClassTx = instA.startFlow(CreateBadgeClass("test", "test", ByteArray(1))).getOrThrow(network)
+        val badgeClassTxB = instB.startFlow(CreateBadgeClass("test1", "test1", ByteArray(1))).getOrThrow(network)
+        val badgeClass = badgeClassTx.output(BadgeClass::class.java)
+        val badgeClassB = badgeClassTxB.output(BadgeClass::class.java)
+        logger.info("BadgeClass Created: {}", badgeClass);
+
+        // issue an Assertion
+        logger.info("Issuing assertion")
+        val assertionTx = instA.startFlow(IssueAssertion(badgeClass.toPointer(), learnerA.info.legalIdentities[0])).getOrThrow(network)
+        val name=badgeClassB.name
+        logger.info("Issuing assertion")
+        val assertionTxB = instB.startFlow(IssueAssertionByName(name, learnerA.info.legalIdentities[0])).getOrThrow(network)
+        val assertionB = assertionTxB.output(Assertion::class.java)
+        logger.info("Assertion issued: {}", assertionB.toString())
+        instB.assertHaveState(assertionB) {
+            s1, s2 ->
+            s1.linearId == s2.linearId
+        }
+        learnerA.assertHaveState(assertionB) {
             s1, s2 ->
             s1.linearId == s2.linearId
         }
