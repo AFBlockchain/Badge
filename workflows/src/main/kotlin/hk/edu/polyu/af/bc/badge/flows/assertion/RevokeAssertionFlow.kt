d7b79658df6abbe9e89326e765538fe56f5f6f0a
YANGZhaochen2002 <93120280+YANGZhaochen2002@users.noreply.github.com>
2022-01-24 17:06:42 +0800

    Update RevokeAssertionFlow.kt
    RevokeAssertionFlow

--- workflows/src/main/kotlin/hk/edu/polyu/af/bc/badge/flows/assertion/RevokeAssertionFlow.kt
+++ workflows/src/main/kotlin/hk/edu/polyu/af/bc/badge/flows/assertion/RevokeAssertionFlow.kt
@@ -1,19 +1,12 @@
 package hk.edu.polyu.af.bc.badge.flows.assertion
 
-
-import com.r3.corda.lib.tokens.contracts.NonFungibleTokenContract
-import com.r3.corda.lib.tokens.contracts.states.EvolvableTokenType
-import com.r3.corda.lib.tokens.contracts.states.NonFungibleToken
-import com.r3.corda.lib.tokens.contracts.types.TokenPointer
-import com.r3.corda.lib.tokens.contracts.types.TokenType
+import com.r3.corda.lib.tokens.contracts.commands.IssueTokenCommand
 import hk.edu.polyu.af.bc.badge.states.Assertion
-import hk.edu.polyu.af.bc.badge.states.BadgeClass
-import jdk.nashorn.internal.ir.annotations.Immutable
-import net.corda.core.contracts.*
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.flows.CollectSignatureFlow
+import net.corda.core.flows.CollectSignaturesFlow
 import net.corda.core.flows.FinalityFlow
 import net.corda.core.flows.FlowLogic
-
 import net.corda.core.node.services.Vault
 import net.corda.core.node.services.queryBy
 import net.corda.core.node.services.vault.QueryCriteria
@@ -21,21 +14,37 @@ import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import java.util.*
 
-
 class RevokeAssertionFlow (
         private var uuid:UUID
         ): FlowLogic<SignedTransaction>() {
     override fun call(): SignedTransaction {
-        val queryCriteria=QueryCriteria.LinearStateQueryCriteria(null, listOf(uuid),null, Vault.StateStatus.UNCONSUMED,null)
+        val queryCriteria= QueryCriteria.LinearStateQueryCriteria(null, listOf(uuid),null, Vault.StateStatus.UNCONSUMED,null)
         val assertionRef =serviceHub.vaultService.queryBy<Assertion>(queryCriteria).states.get(0)
-        val inputAssertion=assertionRef.state.data
-        val notary  = serviceHub.networkMapCache.notaryIdentities[0]
-        val stateAndRef = StateAndRef(TransactionState(inputAssertion,notary=notary), StateRef(inputAssertion.hash(),0))
-        val txBuilder=TransactionBuilder(notary)
-                .addInputState(stateAndRef)
-        txBuilder.verify(serviceHub)
-        val signedTx=serviceHub.signInitialTransaction(txBuilder)
-        return subFlow(FinalityFlow(signedTx))
+
+        val assertion=assertionRef.state.data
+        val newAssertion= Assertion(assertion.badgeClassPointer,
+                assertion.issuer,
+                assertion.participants[0],
+                Date(),  // current time
+                false,
+                UniqueIdentifier())
+
+        val notary=serviceHub.networkMapCache.notaryIdentities.first()
+
+        val transactionBuilder=TransactionBuilder(notary = notary).apply {
+            addInputState(assertionRef)
+            addOutputState(newAssertion)
+            addCommand(IssueTokenCommand(assertion.issuedTokenType, listOf(0)))
+        }
+        transactionBuilder.verify(serviceHub)
+
+        val partSignedTX=serviceHub.signInitialTransaction(transactionBuilder)
+
+        val otherParticipants=ArrayList(newAssertion.participants)
+        otherParticipants.remove(ourIdentity)
+        val otherPartySessions=otherParticipants.map{initiateFlow(it)}
+
+        val fullySignedTx=subFlow(CollectSignaturesFlow(partSignedTX,otherPartySessions))
+        return subFlow(FinalityFlow(fullySignedTx,otherPartySessions))
     }
 }
-
